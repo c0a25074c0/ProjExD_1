02a701785726c523d97cafc5806cb9c4a3431cce
c0a25074c0 <c0a25074c0@edu.teu.ac.jp>
2026-04-14 17:15:38 +0900

    演習3

---
+++ pygame_image.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg") #練習1
+    bg_img2 = pg.transform.flip(bg_img,True,False) #練習8
+    kk_img = pg.image.load("fig/3.png") #練習3
+    kk_img = pg.transform.flip(kk_img,True,False) #練習３
+    kk_rct = kk_img.get_rect() #練習10-1
+    kk_rct.center = 300, 200 #練習10-2
+    tmr = 0
+    while True:
+        x = tmr%3200 #練習5
+        u = 0
+        d = 0
+        r = 0
+        l = 0
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        key_lst = pg.key.get_pressed() #練習10-3
+        
+        if key_lst[pg.K_UP]:
+            u += 1
+        if key_lst[pg.K_DOWN]:
+            d += 1
+        if key_lst[pg.K_LEFT]:
+            l += 1
+        if key_lst[pg.K_RIGHT]:
+            r += 1
+        else:
+            l += 1
+        kk_rct.move_ip((-l+r,-u+d))
+
+        
+        
+        screen.blit(bg_img, [-x, 0]) #練習２
+        screen.blit(bg_img2,[-x+1600,0]) #練習7
+        screen.blit(bg_img,[-x+3200,0]) #練習9
+        screen.blit(kk_img,kk_rct) #練習4→練習10-5
+        pg.display.update()
+        tmr += 1        
+        clock.tick(200)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
